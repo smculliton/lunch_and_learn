0d4ec3738ef59c12635479887a59f49da038a48b
Sean Culliton <smculliton@gmail.com>
2023-01-14 14:09:28 -0700

    Build out RecipesController

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    render json: { thai: 'food' }, status: 200
+    country = params[:country] || CountriesFacade.random_country
+    recipes = RecipesFacade.get_recipes_from_country(country)
+    
+    render json: RecipesSerializer.new(recipes), status: 200
   end
 end
