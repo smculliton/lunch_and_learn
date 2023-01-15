63354ed4b47ee0162b1e33790da102911a7184d8
Sean Culliton <smculliton@gmail.com>
2023-01-14 23:27:38 -0700

    Build get photos method in learningresources facade and tests

--- app/facades/learning_resources_facade.rb
+++ app/facades/learning_resources_facade.rb
@@ -4,4 +4,11 @@ class LearningResourcesFacade
     video_data = data[:items][0]
     Video.new(video_data)
   end
+
+  def self.get_photos(country)
+    photos = UnsplashService.search_photos_by_country(country)[:results]
+    photos.map do |photo_data|
+      Photo.new(photo_data)
+    end
+  end
 end
--- spec/facades/learning_resources_facade_spec.rb
+++ spec/facades/learning_resources_facade_spec.rb
@@ -14,4 +14,21 @@ RSpec.describe LearningResourcesFacade do
       end
     end
   end
+
+  describe '#get_photos' do 
+    it 'returns photos from photo search' do 
+      VCR.use_cassette('photos_from_laos') do 
+        country = 'laos'
+        photos = LearningResourcesFacade.search_photos_by_country(country)
+
+        expect(photos).to be_a Array
+        
+        expect(photos).to all(be_a Photo)
+        photos.each do |photo|
+          expect(photo.alt_tag).to be_a String
+          expect(photo.youtube_video_id).to be_a String
+        end
+      end
+    end
+  end
 end
