4f9c0437830d8926eb0ca86599815dfa764cd757
Sean Culliton <smculliton@gmail.com>
2023-01-14 23:03:41 -0700

    Edit VCR in learning resources facade

--- spec/facades/learning_resources_facade_spec.rb
+++ spec/facades/learning_resources_facade_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe LearningResourcesFacade do 
   describe '#get_video' do
     it 'returns a video object from video API search' do 
-      VCR.use_cassette('videos_about_laos') do
+      VCR.use_cassette('videos_about_laos', allow_playback_repeats: true) do
         country = 'laos'
         videos = YoutubeService.search_videos_by_country(country)
-        video = VideosFacade.get_video(country)
+        video = LearningResourcesFacade.get_video(country)
 
         expect(video).to be_a Video
         expect(video.title).to eq(videos[:items][0][:snippet][:title])
