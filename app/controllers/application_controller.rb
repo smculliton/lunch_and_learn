0841850e4cba8046be8efe4ab65a4e59e58c0901
Sean Culliton <smculliton@gmail.com>
2023-01-17 11:37:57 -0700

    Finish user login path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
-  helper_method :current_user
-
+  
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
