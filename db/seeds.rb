22755a840296ebf39e20c872967ba7bb81b8d261
Sean Culliton <smculliton@gmail.com>
2023-04-11 16:11:27 -0600

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# geordi = User.create!(name: "Geordi Laforge", email: "wassupitsgeordi@enterprise.com", password: 'password', password_confirmation: 'password')
+# geordi.update(api_key: 'geordisapikey')
 
+# shania = User.create!(name: 'Shania Twain', email: 'thechicken@notthesinger.com', password: 'password', password_confirmation: 'password')
+# shania.update(api_key: 'shaniasapikey')
 
-geordi = User.create!(name: "Geordi Laforge", email: "wassupitsgeordi@enterprise.com", password: 'password', password_confirmation: 'password')
-geordi.update(api_key: 'geordisapikey')
+# shania.favorites.create!(country: 'yes please', recipe_link: 'www.grits.com', recipe_title: "Shania's Grits")
+# shania.favorites.create!(country: 'US of A', recipe_link: 'www.chicken-scratch.com', recipe_title: 'Scratch for Your Chickens')
+# shania.favorites.create!(country: 'Zaire', recipe_link: 'www.halloween-recipes.com', recipe_title: 'Left-over Jack-O-Lanterns')
 
-shania = User.create!(name: 'Shania Twain', email: 'thechicken@notthesinger.com', password: 'password', password_confirmation: 'password')
-shania.update(api_key: 'shaniasapikey')
+# sad_boy = User.create!(name: 'Sad Boi George', email: 'ihavenofavorites@recipes.com', password: 'password', password_confirmation: 'password')
+# sad_boy.update(api_key: 'thesaddestkeyever')
 
-shania.favorites.create!(country: 'yes please', recipe_link: 'www.grits.com', recipe_title: "Shania's Grits")
-shania.favorites.create!(country: 'US of A', recipe_link: 'www.chicken-scratch.com', recipe_title: 'Scratch for Your Chickens')
-shania.favorites.create!(country: 'Zaire', recipe_link: 'www.halloween-recipes.com', recipe_title: 'Left-over Jack-O-Lanterns')
-
-sad_boy = User.create!(name: 'Sad Boi George', email: 'ihavenofavorites@recipes.com', password: 'password', password_confirmation: 'password')
-sad_boy.update(api_key: 'thesaddestkeyever')
+guest = User.create!(name: 'Guest', email: 'guest@gmail.com', password: 'password', password_confirmation: 'password')
+guest.update(api_key: 'guest_api_key')
